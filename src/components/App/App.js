b1f705f260abbc316a45d17736c53599d49d4bfb
Myron <dubtwee@gmail.com>
2020-01-06 11:07:37 -0600

    setting up route with home and details page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage/HomePage';
+import DetailsPage from '../pages/DetailsPage/DetailsPage';
 
 class App extends Component {
   // Renders the entire app on the DOM
   render() {
     return (
-      <div className="App">
-        <HomePage />
-      </div>
+      <Router>
+        <div className="App">
+          
+          <Route
+            component={HomePage}
+            exact
+            path="/"
+          />
+          <Route
+            path="/details"
+            component={DetailsPage}
+          />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/DetailsPage/DetailsPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DetailsPage extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Details Page</h2>
+            </div>
+        );
+    }
+}
+
+export default DetailsPage;
